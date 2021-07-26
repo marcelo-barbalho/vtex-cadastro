9df4dc00d48de1dd2317e405defe05ee0ae85752
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2021-07-25 23:17:07 -0300

    correção de rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,6 @@ import { Router, Switch, Route } from "react-router-dom";
 import history from "./config/history";
 import Home from "./views/home/home";
 import Admin from "./views/admin/admin";
-import List from "./views/admin/list";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
@@ -13,7 +12,6 @@ const Routers: React.FC = () => {
       <Header />
       <Switch>
         <Route exact path="/admin" component={Admin} />
-        <Route exact path="/admin/list" component={List} />
         <Route path="/" component={Home} />
       </Switch>
       <Footer />
